f6c8072b3ae380962934363846bd6b2e10ba5427
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2023-05-21 15:18:07 +0900

    Create util

---
+++ src/util/dummy.js
@@ -0,0 +1,32 @@
+export const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "1번 일기",
+    date: 1684420578288,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "2번 일기",
+    date: 1684420578289,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "3번 일기",
+    date: 1684419449437,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "4번 일기",
+    date: 1684419449438,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "5번 일기",
+    date: 1684419449439,
+  },
+];
