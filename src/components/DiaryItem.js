f2ed53a1e7c65d070bc323bed16bfabe2c7e9470
Hyunsoo Kim <hyunsoo99kim@gmail.com>
2023-06-08 13:25:03 +0900

    img error

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -28,7 +28,7 @@ const DiaryItem = ({ id, emotion, content, date }) => {
         ].join(" ")}
       >
         <img
-          src={`https://github.com/D2arDiary/deardiary-fe/tree/main/public/assets/emotion${emotion}.png`}
+          src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}
           alt="emotion"
         />
       </div>
